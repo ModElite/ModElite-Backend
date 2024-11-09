2247320044841a7134b161a4f7cca163d04aab4f
SSSBoOm <boom1909chanapat@gmail.com>
2024-11-09 19:05:45 +0700

    fix: add AllowOrigins

--- server/fiber.go
+++ server/fiber.go
@@ -37,7 +37,7 @@ func (s *FiberServer) Start() {
 	})
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173,http://localhost:3000",
+		AllowOrigins:     "http://localhost:5173,http://localhost:3000,https://test.sssboom.xyz",
 		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
 		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH",
 		AllowCredentials: true,
