1532942e3d55f0acf6a313087edeff6cb1247b42
SSSBoOm <boom1909chanapat@gmail.com>
2024-11-10 18:36:55 +0700

    fix: update foreign key reference in thai_subdistricts migration

--- db/migrations/000010_create_thai_subdistricts.up.sql
+++ db/migrations/000010_create_thai_subdistricts.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS "thai_subdistricts" (
   id UUID NOT NULL PRIMARY KEY,
   zip_code INT NOT NULL,
   name_th VARCHAR(150) NOT NULL,
-  amphure_id UUID NOT NULL,
-  FOREIGN KEY (amphure_id) REFERENCES thai_amphures(id)
+  provinces_id UUID NOT NULL,
+  FOREIGN KEY (provinces_id) REFERENCES thai_provinces(id)
 );
