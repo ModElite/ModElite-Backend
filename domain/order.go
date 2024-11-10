2786cb20a57c659a058b07e2a881f8abf3563f77
SSSBoOm <boom1909chanapat@gmail.com>
2024-11-10 13:39:12 +0700

    feat: domain order, order_product

---
+++ domain/order.go
@@ -0,0 +1,31 @@
+package domain
+
+import "time"
+
+type OrderStatusType string
+
+const (
+	ORDER_PENDING         OrderStatusType = "PENDING"
+	ORDER_PAYMENT_SUCCESS OrderStatusType = "PAYMENT_SUCCESS"
+	ORDER_REFUND          OrderStatusType = "REFUND"
+	ORDER_END             OrderStatusType = "END"
+	ORDER_CANCEL          OrderStatusType = "CANCEL"
+)
+
+type Order struct {
+	ID             string          `json:"id" db:"id"`
+	USER_ID        string          `json:"userId" db:"user_id"`
+	STATUS         OrderStatusType `json:"status" db:"status"`
+	TOTAL_PRICE    float64         `json:"totalPrice" db:"total_price"`
+	PRODUCT_PRICE  float64         `json:"productPrice" db:"product_price"`
+	SHIPPING_PRICE float64         `json:"shippingCost" db:"shipping_cost"`
+	ADDRESS        string          `json:"address" db:"address"`
+	CREATED_AT     time.Time       `json:"createdAt" db:"created_at"`
+	UPDATED_AT     time.Time       `json:"updatedAt" db:"updated_at"`
+}
+
+type OrderRepository interface {
+}
+
+type OrderUsecase interface {
+}
---
+++ domain/order_product.go
@@ -0,0 +1,28 @@
+package domain
+
+import "time"
+
+type OrderProductStatus string
+
+const (
+	ORDER_PRODUCT_PENDING             OrderProductStatus = "PENDING"
+	ORDER_PRODUCT_DELIVERY_ON_THE_WAY OrderProductStatus = "DELIVERY_ON_THE_WAY"
+	ORDER_PRODUCT_DELIVERED                              = "DELIVERED"
+)
+
+type OrderProduct struct {
+	ID              string             `json:"id" db:"id"`
+	ORDER_ID        string             `json:"orderId" db:"order_id"`
+	PRODUCT_SIZE_ID string             `json:"productSizeId" db:"product_size_id"`
+	STATUS          OrderProductStatus `json:"status" db:"status"`
+	QUANTITY        int                `json:"quantity" db:"quantity"`
+	PRICE           int                `json:"price" db:"price"`
+	CREATED_AT      time.Time          `json:"createdAt" db:"created_at"`
+	UPDATED_AT      time.Time          `json:"updatedAt" db:"updated_at"`
+}
+
+type OrderProductRepository interface {
+}
+
+type OrderProductUsecase interface {
+}
