3908745880967df751c9373f457efff43f1b1ff4
SSSBoOm <boom1909chanapat@gmail.com>
2024-11-14 00:51:11 +0700

    feat: cart sql migrations

---
+++ db/migrations/000011_create_cart.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS "cart" (
+  id SERIAL PRIMARY KEY,
+  user_id UUID NOT NULL,
+  product_size_id UUID NOT NULL,
+  quantity INT NOT NULL DEFAULT 1,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES "users"(id),
+  FOREIGN KEY (product_size_id) REFERENCES "product_size"(id)
+);
